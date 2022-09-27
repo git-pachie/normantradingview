be0f1f6ae3de08fd13b2d70fade02e49833637a0
archieangeleshackint <archieangeleshackint@PACHIEHACKIFE46>
2022-09-28 03:47:29 +0800

    Add project files.

---
+++ WebApplication1/Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var market = HttpContext.Request.Query["Market"];
+            var symbol = HttpContext.Request.Query["Symbol"];
+
+            if(string.IsNullOrEmpty(market))
+            {
+                market = "NASDAQ";
+            }
+
+
+            if (string.IsNullOrEmpty(symbol))
+            {
+                symbol = "AAPL";
+            }
+
+
+            var model = new Models.Chart
+            {
+                Market = market
+            ,
+                Symbol = symbol
+                , MarketSymbol = $"{market}:{symbol}"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Models/Chart.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Chart
+    {
+        public string Market { get; set; }
+        public string Symbol { get; set; }
+
+        public string  MarketSymbol  { get; set; }
+    }
+}
